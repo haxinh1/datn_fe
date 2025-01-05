57c371fa4158318d9c1fb242e04172b8c3f8a118
suycode <lebaongoc1652004@gmail.com>
2025-01-06 00:16:30 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,38 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Admin from './pages/Admin'
-import Add from './pages/Add'
-import Edit from './pages/Edit'
-import Home from './pages/Home'
-import LayoutAdmin from './layout/LayoutAdmin'
-import Detail from './pages/Detail'
-import { useEffect, useState } from 'react'
-import api from './axios'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Cart from './pages/Cart'
-import Pay from './pages/Pay'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import List from "./admin/product/List";
+import Edit from "./admin/product/Edit";
+import Add from "./admin/product/Add";
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Pay from './pages/Pay';
+import Detail from './pages/Detail';
+import LayoutAdmin from "./layout/LayoutAdmin";
 
 function App() {
-  const [products, setProducts] = useState([]);
 
-	useEffect(() => {
-		(async () => {
-			try {
-				const { data } = await api.get("/products");
-				setProducts(data);
-			} catch (error) {
-				console.log(error);
-			}
-		})();
-	}, []);
-  
   return (
     <>
+
       <Routes>
-        <Route path="/" element={<LayoutAdmin/>}>
-          <Route index element={<Home data={products}/>}/>
-          <Route path="/detail/:id" element={<Detail/>}/>
-          <Route path="/admin" element={<Admin/>}/>
-          <Route path="/add" element={<Add/>}/>
-          <Route path="/edit/:id" element={<Edit/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/cart" element={<Cart/>}/>
-          <Route path="/pay" element={<Pay/>}/>
+        <Route path="/" element={<LayoutAdmin />}>
+          <Route path="/list-pr" element={<List />} />
+          <Route path="/add-pr" element={<Add />} />
+          <Route path="/edit-pr" element={<Edit />} />
+        </Route>
+      </Routes>
+
+      <Routes>
+        <Route path="/" element={<LayoutAdmin />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/pay" element={<Pay />} />
+          <Route path="/detail-pr" element={<Detail />} />
+  
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
