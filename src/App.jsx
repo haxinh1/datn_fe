26e49734d1949cfb135d746c786f37849fb600e9
anhhoang104 <hanh104204@gmail.com>
2025-01-14 22:06:31 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,7 @@ import { useEffect, useState } from "react";
 import api from "./axios";
 import LayoutAdmin from "./Layout/LayoutAdmin";
 
-import List from "./Admin/product/List";
-import Edit from "./Admin/product/Edit";
-import Add from "./Admin/product/Add";
-import ProductDetail from "./Admin/product/DetailAd";
-// import Register from "./admin/Register";
-// import Login from "./admin/Login";
-// import HomePage from "./pages/HomePage";
-// import Cart from "./pages/Cart";
-// import Pay from "./pages/Pay";
-// import Detail from "./pages/Detail";
-// import Signin from "./pages/Signin";
-// import Signup from "./pages/Signup";
-// import Bill from "./admin/Bill";
-// import Inbox from "./admin/Inbox";
+import ProductDetail from "./admin/product/DetailAd";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -38,9 +25,9 @@ function App() {
       <Routes>
         <Route path="/" element={<LayoutAdmin />}>
           <Route path="/detailad" element={<ProductDetail />} />
-          <Route path="/list-pr" element={<List />} />
+          {/* <Route path="/list-pr" element={<List />} />
           <Route path="/add-pr" element={<Add />} />
-          <Route path="/edit-pr" element={<Edit />} />
+          <Route path="/edit-pr" element={<Edit />} /> */}
           {/* <Route path="/bill" element={<Bill />} /> */}
           {/* <Route path="/inbox" element={<Inbox />} />
           <Route path="/register" element={<Register />} />
