435c4be7ab1bc1b782ce8a6a9d1b7571145dc463
tuyetmai03 <tuyetmai03nd@gmail.com>
2025-01-15 17:06:12 +0700

    fix(admin): react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LayoutAdmin from "./Layout/LayoutAdmin";
 import Categories from "./admin/category";
 
@@ -6,13 +6,13 @@ function App() {
 
   return (
     <>
-    <Router>
+    
       <Routes>
         <Route path="/" element={<LayoutAdmin />}>
           <Route path="/categories" element={<Categories />} />
         </Route>
       </Routes>
-    </Router>
+    
     </>
   );
 }
