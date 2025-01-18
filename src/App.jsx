bfc675d6f72b4fb2ec23acc0da1f227ae98c003a
anhhoang104 <hanh104204@gmail.com>
2025-01-18 22:52:56 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
 import api from "./axios";
 import LayoutAdmin from "./layout/LayoutAdmin";
-import ProductDetail from "./Admin/product/DetailAd";
-import Categories from "./Admin/category";
-
+import ProductDetail from "./admin/product/DetailAd";
 
 function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const { data } = await api.get("/products");
-        setProducts(data);
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-  }, []);
-
   return (
     <>
       <Routes>
         <Route path="/" element={<LayoutAdmin />}>
           <Route path="/detailad" element={<ProductDetail />} />
-          <Route path="/categories" element={<Categories />} />
-          {/* <Route path="/list-pr" element={<List />} />
-          <Route path="/add-pr" element={<Add />} />
-          <Route path="/edit-pr" element={<Edit />} /> */}
-          {/* <Route path="/bill" element={<Bill />} /> */}
-          {/* <Route path="/inbox" element={<Inbox />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} /> */}
         </Route>
       </Routes>
-
     </>
   );
 }
