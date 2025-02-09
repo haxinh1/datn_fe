5c0401fee1784cdabb2a3a55992ff81e3aed1264
anhhoang104 <hanh104204@gmail.com>
2025-02-09 22:56:43 +0700

    update 9/2

--- src/services/attribute_value.js
+++ src/services/attribute_value.js
@@ -1,23 +1,26 @@
 import instance from "../axios"; // axios instance với base URL
 
 const fetchValue = async () => {
-    const response = await instance.get("/attributes_values");
-    return response.data; // Trả về data từ BE
+  const response = await instance.get("/attributes_values");
+  return response.data.map((value) => ({
+    ...value,
+    attribute_name: value.attribute_id === 1 ? "Màu" : "Size", // Tùy chỉnh theo bảng của bạn
+  })); // Trả về data từ BE
 };
 
 const createValue = async (payload) => {
-    const response = await instance.post("/attributes_values", payload);
-    return response.data;
+  const response = await instance.post("/attributes_values", payload);
+  return response.data;
 };
 
 const deleteValue = async (id) => {
-    const response = await instance.delete(`/attributes_values/${id}`);
-    return response.data;
+  const response = await instance.delete(`/attributes_values/${id}`);
+  return response.data;
 };
 
 // Xuất các hàm để dùng trong các component
 export const AttributesServices = {
-    fetchValue,
-    createValue,
-    deleteValue
+  fetchValue,
+  createValue,
+  deleteValue,
 };
