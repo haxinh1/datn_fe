62f2c37c75a9f66dc23137193f1a6c4094d138b2
anhhoang104 <hanh104204@gmail.com>
2025-04-04 17:23:31 +0700

    dashboard

---
+++ src/services/statisticServices.js
@@ -0,0 +1,82 @@
+import instance from "../axios";
+
+//top 10 người dùng mua nhiều nhất
+const fetchTopUserBought = async () => {
+  try {
+    const { data } = await instance.get("/statistics/top-user-bought");
+    return data.topUserBought;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//top sản phẩm bán ra nhiều nhất
+const fetchTopProductBought = async (filter = "quantity") => {
+  try {
+    const { data } = await instance.get("/statistics/top-product-bought", {
+      params: { filter },
+    });
+    return data.topProductBought;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//doanh thu theo ngày
+const fetchRevenue = async () => {
+  try {
+    const { data } = await instance.get("/statistics/revenue");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//tổng đơn theo trạng thái
+const fetchOrderStatistics = async () => {
+  try {
+    const { data } = await instance.get("/statistics/order-statistics");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//top sản phẩm được mua và xem
+const fetchTopBuyView = async () => {
+  try {
+    const { data } = await instance.get("/statistics/top-buy-view");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//biểu đồ hoàn hủy
+const fetchRevenueStatistics = async () => {
+  try {
+    const { data } = await instance.get("/statistics/revenue-statistics");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+//top doanh thu cao nhất theo ngày
+const fetchTopRevenueDays = async () => {
+  try {
+    const { data } = await instance.get("/statistics/top-revenue-days");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const statisticServices = {
+  fetchTopUserBought,
+  fetchTopProductBought,
+  fetchRevenue,
+  fetchOrderStatistics,
+  fetchTopBuyView,
+  fetchRevenueStatistics,
+  fetchTopRevenueDays,
+};
