53b1406ddc04b37183e9999c36d8f99ab09dbacd
tuyetmai03 <tuyetmai03nd@gmail.com>
2025-04-05 23:05:45 +0700

    feat: chat real-time

---
+++ src/echo.js
@@ -0,0 +1,20 @@
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+
+window.Pusher = Pusher;
+
+const echo = new Echo({
+  broadcaster: "pusher",
+  key: "6b2509032695e872d989",
+  cluster: "ap1",
+  forceTLS: true,
+  encrypted: true,
+  authEndpoint: "http://127.0.0.1:8000/broadcasting/auth",
+  auth: {
+    headers: {
+      Authorization: 'Bearer ' + localStorage.getItem('client_token'), 
+    },
+  },
+});
+
+export default echo;
